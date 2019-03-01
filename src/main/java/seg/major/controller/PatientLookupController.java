ce9fe95264e7c20f00bfb505368738e3eb3ae454
Michael Jacob <mottijey@aol.com>
2019-03-01 15:49:43 +0000

    Add patient lookup functionality

---
+++ src/main/java/seg/major/controller/PatientLookupController.java
@@ -0,0 +1,83 @@
+package seg.major.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import seg.major.model.PatientLookupModel;
+
+import seg.major.structure.Patient;
+
+import java.util.List;
+
+public class PatientLookupController {
+    private PatientLookupModel patientModel;
+    public TextField searchField;
+    public Button searchButton;
+    public Button filterButton;
+    public TableView patientTable;
+    public TableColumn forename;
+    public TableColumn surname;
+    public TableColumn hospitalNumber;
+    public TableColumn local;
+    public TableColumn nextAppointment;
+    public Button under12Button;
+    public Button over12Button;
+
+    public PatientLookupController() {
+        this.patientModel = new PatientLookupModel();
+        patientModel.fetchData();
+        setButtons();
+        fillTable();
+    }
+
+    private void setButtons() {
+        under12Button.setOnAction(e -> {
+            fillTable(patientModel.under12());
+        });
+        over12Button.setOnAction(e -> {
+            fillTable(patientModel.over12());
+        });
+        searchButton.setOnAction(e -> {
+            search(searchField.getText());
+        });
+        //TODO filter button
+    }
+
+    private void fillTable(){
+        // may need to assign column to attributes using setCellValueFactory
+        patientTable.getItems().clear();
+        for (Patient patient : patientModel.getPatientList()) {
+            patientTable.getItems().add(patient);
+        }
+    }
+
+    private void fillTable(List<Patient> patients){
+        patientTable.getItems().clear();
+        for (Patient patient : patients) {
+            patientTable.getItems().add(patient);
+        }
+    }
+
+    private void search(String searchString){
+        if (searchString.matches(".*\\d+.*")) {
+            searchNumber(searchString);
+        }
+        else{
+            searchName(searchString);
+        }
+    }
+
+    private void searchNumber(String number) {
+        fillTable(patientModel.searchByNumber(number));
+    }
+
+    private void searchName(String name){
+        fillTable(patientModel.searchByName(name));
+    }
+
+    public void refresh(){
+        patientModel.fetchData();
+        fillTable();
+    }
+}
---
+++ src/main/java/seg/major/model/PatientLookupModel.java
@@ -0,0 +1,74 @@
+package seg.major.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import seg.major.structure.Patient;
+
+public class PatientLookupModel {
+
+    private List<Patient> patientList;
+
+    public List<Patient> getPatientList() {
+        return patientList;
+    }
+
+    private static void openConnection() {
+
+    }
+
+    public List fetchData() {
+        openConnection();
+
+
+        //TODO:
+
+        patientList = new ArrayList<Patient>();
+
+
+        closeConnection();
+        return patientList;
+    }
+
+    private static void closeConnection() {
+    }
+
+    /*
+     * search and filter methods
+     * TODO handle failed searches
+     *
+     */
+    public List searchByName(String name){
+        List<Patient> filtered = new ArrayList<Patient>();
+        patientList.stream()
+                .filter(p -> (p.getForename() + " " + p.getSurname()).contains(name))
+                .forEach(p -> filtered.add(p));
+        return filtered;
+    }
+
+    public List searchByNumber(String number){
+        List<Patient> filtered = new ArrayList<Patient>();
+        patientList.stream()
+                .filter(p -> p.getHospitalNumber().contains(number))
+                .forEach(p -> filtered.add(p));
+        return filtered;
+    }
+
+    public List over12(){
+        List<Patient> filtered = new ArrayList<Patient>();
+        patientList.stream()
+                .filter(p -> p.getDob().plusYears(12).isAfter(LocalDate.now()) ||  p.getDob().plusYears(12).isEqual(LocalDate.now()))
+                .forEach(p -> filtered.add(p));
+        return filtered;
+    }
+
+    public List under12(){
+        List<Patient> filtered = new ArrayList<Patient>();
+        patientList.stream()
+                .filter(p -> p.getDob().plusYears(12).isBefore(LocalDate.now()))
+                .forEach(p -> filtered.add(p));
+        return filtered;
+    }
+
+}
