6418e253c333122c9c7b2d1a72aecceb36565538
Tom <31593883+TomRYH@users.noreply.github.com>
2019-03-02 20:09:59 +0000

    Add method to return pane of the contents

--- src/main/java/seg/major/controller/LoginController.java
+++ src/main/java/seg/major/controller/LoginController.java
@@ -1,18 +1,39 @@
 package seg.major.controller;
 
+import java.util.ResourceBundle;
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import seg.major.model.LoginModel;
+import javafx.fxml.FXMLLoader;
+import javafx.event.*;
 
-public class LoginController {
+public class LoginController implements Initializable {
+
+    @FXML
     public TextField username;
+    @FXML
     public Button login_button;
+    @FXML
     public Button register_button;
+    @FXML
     public PasswordField password;
-    private LoginModel model;
-    public LoginController() {
-        model = new LoginModel();
-        model.validateLogin(username.getText(), password.getText());
+    public static LoginModel loginModel;
+
+    public void initialize(URL url, ResourceBundle rb) {
     }
+
+    public AnchorPane getPane() throws Exception {
+        AnchorPane pane = FXMLLoader.load(getClass().getClassLoader().getResource("views/login.fxml"));
+        return pane;
+    }
+
 }
