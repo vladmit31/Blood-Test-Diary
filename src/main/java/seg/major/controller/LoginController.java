bb6d59b09b597a71dee1f27e0920d2039c66840c
Yinuo Xiang <p199729@gmail.com>
2019-03-03 12:47:43 +0000

    Do some works on LoginController and Login Button. Change the view of login View.

--- src/main/java/seg/major/controller/LoginController.java
+++ src/main/java/seg/major/controller/LoginController.java
@@ -1,6 +1,8 @@
 package seg.major.controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import seg.major.model.LoginModel;
@@ -8,11 +10,28 @@ import seg.major.model.LoginModel;
 public class LoginController {
     public TextField username;
     public Button login_button;
-    public Button register_button;
     public PasswordField password;
+    public Label message;
     private LoginModel model;
+
     public LoginController() {
+        /*
+        username = new TextField();
+        login_button = new Button();
+        password = new PasswordField();
+        message = new Label();
         model = new LoginModel();
-        model.validateLogin(username.getText(), password.getText());
+        */
+
+    }
+
+    @FXML
+    private void handleLoginButton() {
+        if(model.validateLogin(username.getText(), password.getText())) {
+
+            message.setVisible(false);
+        }else {
+            message.setVisible(true);
+        }
     }
 }
