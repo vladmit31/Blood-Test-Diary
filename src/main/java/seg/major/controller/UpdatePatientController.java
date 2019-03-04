b7edc41fe13d4b6ea605b49846c5382d2170d7cf
Vlad <vladmititelu1@gmail.com>
2019-03-04 20:49:33 +0000

    Create controller and model for update patient view

--- src/main/java/seg/major/controller/UpdatePatientController.java
+++ src/main/java/seg/major/controller/UpdatePatientController.java
@@ -1,28 +1,32 @@
 package seg.major.controller;
 
-import java.util.ResourceBundle;
-import java.net.URL;
-import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import seg.major.database.DatabaseConnection;
-import seg.major.model.AddPatientModel;
+import seg.major.model.UpdatePatientModel;
 import seg.major.structure.Patient;
 
 import javax.swing.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * AddPatientController acts as the controller for the add_patient.fxml file
  */
-public class AddPatientController implements Initializable, ViewsController {
+public class UpdatePatientController implements Initializable, ViewsController {
 
-  private PrimaryController primaryController;
+    private PrimaryController primaryController;
 
-  /** ---------- FXML ---------- */
-  
+    /** ---------- FXML ---------- */
+
+    @FXML
+    private Text TitleText;
+
     @FXML
     private TextField forenameField;
 
@@ -45,28 +49,29 @@ public class AddPatientController implements Initializable, ViewsController {
     private Button cancelButton;
 
     @FXML
-    private Button submitButton;
+    private Button updateButton;
 
-    private AddPatientModel model;
+    private UpdatePatientModel model;
 
-  /** ---------- FXML ---------- */
-  
-  @FXML
-    public void submit()
+    /** ---------- FXML ---------- */
+
+    @FXML
+    public void update()
     {
         if(checkUserInput()) {
-            Patient newPatient = model.createPatient(forenameField.getText(), surnameField.getText(),
+            Patient newPatient = model.updatePatient(forenameField.getText(), surnameField.getText(),
                     dobField.getValue(), hospitalField.getText(), clinicField.getText(), nextAppField.getValue());
 
-            DatabaseConnection.insertPatient(newPatient);
+            //DatabaseConnection.insertPatient(newPatient);
+            // need to create updatePatient() method in DatabaseConnection class
         }
         else{
             JOptionPane.showMessageDialog(null,
                     "Complete all fields", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-  
-   @FXML
+
+    @FXML
     public void cancel(){
         // TODO: Link back to the patient panel
         Stage stage = (Stage) cancelButton.getScene().getWindow();
@@ -79,22 +84,24 @@ public class AddPatientController implements Initializable, ViewsController {
                 !hospitalField.getText().equals("") && !clinicField.getText().equals("") &&  nextAppField.getValue()!=null;
     }
 
-  /** ---------- Inherited / Implemented ---------- */
-  /**
-   * Allow javafx to initalise the controller with the view
-   */
-  public void initialize(URL url, ResourceBundle rb) {
-    model = new AddPatientModel();
-  }
+    /** ---------- Inherited / Implemented ---------- */
+    /**
+     * Allow javafx to initalise the controller with the view
+     */
+    public void initialize(URL url, ResourceBundle rb) {
+        model = new UpdatePatientModel();
+        TitleText.setText("Update patient data: " /* +rb.getString()*/);
 
-  /**
-   * Set the primaryController
-   * 
-   * @param primaryController the PrimaryController to set
-   */
-  public void setScreenParent(PrimaryController primaryController) {
-    this.primaryController = primaryController;
-  }
-  /** ---------- Inherited / Implemented ---------- */
- 
+    }
+
+    /**
+     * Set the primaryController
+     *
+     * @param primaryController the PrimaryController to set
+     */
+    public void setScreenParent(PrimaryController primaryController) {
+        this.primaryController = primaryController;
+    }
+    /** ---------- Inherited / Implemented ---------- */
+
 }
