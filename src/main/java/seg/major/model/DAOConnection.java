ca080c1dba2dbe85eaf8a5ee5b0fed178efb369c
Tom <31593883+TomRYH@users.noreply.github.com>
2019-03-07 08:52:11 +0000

    Remove redundant execute method

--- src/main/java/seg/major/model/DAOConnection.java
+++ src/main/java/seg/major/model/DAOConnection.java
@@ -12,23 +12,6 @@ public class DAOConnection {
     private static final String PASSWORD = App.props.getProperty("db_password");
     private static final String DB_NAME = App.props.getProperty("db_name");
 
-    public static void execute(String command) {
-
-        try {
-            Connection con = getConnection();
-            Statement stt = con.createStatement();
-            stt.execute("USE " + DB_NAME + ";");
-            try {
-                stt.execute(command);
-            } finally {
-                stt.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
