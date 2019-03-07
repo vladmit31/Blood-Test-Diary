b5e47df12fb6325aff5e3ac88e6e83555c46880f
Tom <31593883+TomRYH@users.noreply.github.com>
2019-03-07 03:12:07 +0000

    Rename ViewController

--- src/main/java/seg/major/controller/ViewsController.java
+++
@@ -1,9 +0,0 @@
-package seg.major.controller;
-
-/**
- * Interface to allow all view controllers to share the PrimaryController
- */
-public interface ViewsController {
-  public void setScreenParent(PrimaryController primaryController);
-
-}
