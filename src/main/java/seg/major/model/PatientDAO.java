bb536e0acbaa24f38f69253938eb78f1e806c5a1
Tom <31593883+TomRYH@users.noreply.github.com>
2019-03-07 03:17:32 +0000

    Skeleton patient DAO

---
+++ src/main/java/seg/major/model/PatientDAO.java
@@ -0,0 +1,82 @@
+package seg.major.model;
+
+import java.util.Map;
+
+import seg.major.structure.Patient;
+
+public class PatientDAO implements DAOInterface<Patient> {
+
+    public PatientDAO() {
+
+    }
+
+    /** ---------- Inherited / Implemented ---------- */
+    /**
+     * Execute a command through the data source
+     * 
+     * @param command the command to be executed
+     */
+    public void execute(String command) {
+    };
+
+    /**
+     * @return how many records are stored
+     */
+    public int size() {
+        return 0;
+    };
+
+    /**
+     * Lookup a record by ID
+     * 
+     * @param toGet the ID to lookup and fetch the record for
+     */
+    public Patient getById(int toGet) {
+        return null;
+    };
+
+    /**
+     * Remove a record by ID
+     * 
+     * @param toGet the ID to remove
+     */
+    public void removeById(int toRemove) {
+    };
+
+    /**
+     * @param toCreate the patient to create as a record
+     */
+    public void create(Patient toCreate) {
+    };
+
+    /**
+     * @param toGet the ID of the record
+     * @return the patient corresponding to the record
+     */
+    public Patient get(int toGet) {
+        return null;
+    };
+
+    /**
+     * @param toUpdate the patient to update
+     */
+    public void update(Patient toUpdate) {
+    }
+
+    /**
+     * @param toRemove the patient to remove
+     */
+    public void remove(Patient toRemove) {
+    };
+
+    /**
+     * @param toGet Map of atttributes and the values to match to a record
+     * @return the matched patient
+     */
+    public Patient get(Map<String, String> toGet) {
+        return null;
+    };
+
+    /** ---------- Inherited / Implemented ---------- */
+
+}
