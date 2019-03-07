a3d9a339e2ff3460dd374362ae5d3beae02ae37e
Tom <31593883+TomRYH@users.noreply.github.com>
2019-03-07 03:12:48 +0000

    Formatting

--- src/main/java/seg/major/model/AddPatientModel.java
+++ src/main/java/seg/major/model/AddPatientModel.java
@@ -1,20 +1,17 @@
 package seg.major.model;
-import seg.major.structure.Patient;
 
 import java.time.LocalDate;
 
+import seg.major.structure.Patient;
 
 public class AddPatientModel {
+  public AddPatientModel() {
+  }
 
-    public AddPatientModel(){
+  public Patient createPatient(String forename, String surname, LocalDate dob, String hospitalNumber,
+      String localClinic, LocalDate nextAppointment) {
+    Patient patient = new Patient(forename, surname, dob, hospitalNumber, localClinic, nextAppointment);
+    return patient;
 
-    }
-
-    public Patient createPatient(String forename, String surname, LocalDate dob, String hospitalNumber, String localClinic, LocalDate nextAppointment)
-    {
-        Patient patient = new Patient(forename, surname, dob, hospitalNumber, localClinic, nextAppointment);
-        return patient;
-
-    }
-
+  }
 }
