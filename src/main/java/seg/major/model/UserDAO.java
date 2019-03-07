f80df80e8e8954e596ee7e0e1fa925e9421db590
Tom <31593883+TomRYH@users.noreply.github.com>
2019-03-07 08:53:32 +0000

    Implement interface methods and queries

--- src/main/java/seg/major/model/UserDAO.java
+++ src/main/java/seg/major/model/UserDAO.java
@@ -1,5 +1,6 @@
 package seg.major.model;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,69 +18,164 @@ public class UserDAO implements DAOInterface<User> {
   private static final String IS_ADMIN = "is_admin";
 
   public UserDAO() {
-
   }
 
   /** ---------- Inherited / Implemented ---------- */
 
   /**
-   * Execute a command through the data source
-   * 
-   * @param command the command to be executed
-   */
-  public void execute(String command) {
-    DAOConnection.execute(command);
-  };
-
-  /**
-   * @return how many records are stored
-   */
-  public int size() {
-    return 0;
-  };
-
-  /**
    * Lookup a record by ID
    * 
-   * @param toGet the ID to lookup and fetch the record for
+   * @param toGet the ID of the record
+   * @return the user corresponding to the record
    */
   public User getById(int toGet) {
-    return null;
-  };
+    return get(toGet);
+  }
 
   /**
    * Remove a record by ID
    * 
-   * @param toGet the ID to remove
+   * @param toRemove the ID to remove
    */
   public void removeById(int toRemove) {
-  };
+    String query = "DELETE FROM user WHERE id = ? LIMIT 1;";
+    PreparedStatement ps = null;
+    Connection conn = null;
+    try {
+      conn = DAOConnection.getConnection();
+      ps = conn.prepareStatement(query);
+      ps.setInt(1, toRemove);
+      ps.execute("USE db");
+      ps.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        ps.close();
+      } catch (Exception e) {
+      }
+      try {
+        conn.close();
+      } catch (Exception e) {
+      }
+    }
+  }
 
   /**
    * @param toCreate the user to create as a record
    */
   public void create(User toCreate) {
-  };
+    String query = "INSERT INTO user (username, email, password, is_admin) VALUES (?, ?, ?, ?)";
+    PreparedStatement ps = null;
+    Connection conn = null;
+    try {
+      conn = DAOConnection.getConnection();
+      ps = conn.prepareStatement(query);
+      ps.setString(1, toCreate.getUsername());
+      ps.setString(2, toCreate.getEmail());
+      ps.setString(3, toCreate.getPassword());
+      ps.setInt(4, toCreate.getIsAdmin());
+      ps.execute("USE db");
+      ps.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+
+      try {
+        ps.close();
+      } catch (Exception e) {
+      }
+      try {
+        conn.close();
+      } catch (Exception e) {
+      }
+    }
+
+  }
 
   /**
    * @param toGet the ID of the record
    * @return the user corresponding to the record
    */
   public User get(int toGet) {
-    return null;
-  };
+    String query = "SELECT * FROM user WHERE id = ? LIMIT 1;";
+    User toReturn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    Connection conn = null;
+    try {
+      conn = DAOConnection.getConnection();
+      ps = conn.prepareStatement(query);
+      ps.setInt(1, toGet);
+      ps.execute("USE db");
+      rs = ps.executeQuery();
+      toReturn = resultSetToUser(rs);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        rs.close();
+      } catch (Exception e) {
+      }
+      try {
+        ps.close();
+      } catch (Exception e) {
+      }
+      try {
+        conn.close();
+      } catch (Exception e) {
+      }
+    }
+
+    return toReturn;
+  }
 
   /**
    * @param toUpdate the user to update
    */
   public void update(User toUpdate) {
+    String query = "UPDATE user SET username = ?, email = ?, password = ?, is_admin ?  WHERE id = ? AND email = ?";
+    User toReturn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    Connection conn = null;
+    try {
+      conn = DAOConnection.getConnection();
+      ps = conn.prepareStatement(query);
+      ps.setString(1, toUpdate.getUsername());
+      ps.setString(2, toUpdate.getEmail());
+      ps.setString(3, toUpdate.getPassword());
+      ps.setInt(4, toUpdate.getIsAdmin());
+      ps.setInt(5, toUpdate.getID());
+      ps.setString(6, toUpdate.getEmail());
+      ps.execute("USE db");
+      rs = ps.executeQuery();
+      toReturn = resultSetToUser(rs);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        rs.close();
+      } catch (Exception e) {
+      }
+      try {
+        ps.close();
+      } catch (Exception e) {
+      }
+      try {
+        conn.close();
+      } catch (Exception e) {
+      }
+    }
+
   }
 
   /**
    * @param toRemove the user to remove
    */
   public void remove(User toRemove) {
-  };
+    removeById(toRemove.getID());
+  }
 
   /**
    * @param toGet Map of atttributes and the values to match to a record
@@ -87,7 +183,7 @@ public class UserDAO implements DAOInterface<User> {
    */
   public User get(Map<String, String> toGet) {
     return null;
-  };
+  }
 
   /** ---------- Inherited / Implemented ---------- */
 
@@ -98,28 +194,73 @@ public class UserDAO implements DAOInterface<User> {
    * @return the user that was found
    */
   public static User getByUsername(String toGet) {
-    String query = " SELECT * FROM user WHERE username = ?";
-
+    String query = "SELECT * FROM user WHERE username = ? LIMIT 1;";
+    User toReturn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    Connection conn = null;
     try {
-      PreparedStatement s = DAOConnection.getConnection().prepareStatement(query);
-      s.setString(1, toGet);
-      s.execute("USE db");
-      ResultSet res = s.executeQuery();
-
-      while (res.next()) {
-        int id = res.getInt(ID);
-        String username = res.getString(USERNAME);
-        String password = res.getString(PASSWORD);
-        String email = res.getString(EMAIL);
-        int is_admin = res.getInt(IS_ADMIN);
-        return new User(id, username, email, password, is_admin);
-      }
+      conn = DAOConnection.getConnection();
+      ps = conn.prepareStatement(query);
+      ps.setString(1, toGet);
+      ps.execute("USE db");
+      rs = ps.executeQuery();
+      toReturn = resultSetToUser(rs);
     } catch (SQLException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
+    } finally {
+      try {
+        rs.close();
+      } catch (Exception e) {
+      }
+      try {
+        ps.close();
+      } catch (Exception e) {
+      }
+      try {
+        conn.close();
+      } catch (Exception e) {
+      }
     }
 
-    return null;
+    return toReturn;
+  }
+
+  /**
+   * Get the user the ResultSet pointer currently points to
+   * 
+   * @param toConvert the ResultSet to be read
+   * @return the user
+   */
+  private static User resultSetToUser(ResultSet toConvert) throws SQLException {
+    if (toConvert.next()) {
+      int id = toConvert.getInt(ID);
+      String username = toConvert.getString(USERNAME);
+      String password = toConvert.getString(PASSWORD);
+      String email = toConvert.getString(EMAIL);
+      int isAdmin = toConvert.getInt(IS_ADMIN);
+      return new User(id, username, email, password, isAdmin);
+
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Convert a ResultSet to an array containing all records contained
+   * 
+   * @param toConvert the ResultSet to be read
+   * @return the users
+   */
+  private User[] resultSetToUserArray(ResultSet toConvert) throws SQLException {
+    User[] toReturn = new User[toConvert.getFetchSize()];
+    int i = 0;
+    while (toConvert.next()) {
+      toReturn[i] = resultSetToUser(toConvert);
+      i++;
+    }
+
+    return toReturn;
   }
 
 }
