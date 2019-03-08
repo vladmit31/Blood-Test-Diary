6ee7488ebf4b988659fc7c3100d156778e43385b
Horia Pavel <horiapavel@yahoo.com>
2019-03-08 15:34:47 +0000

    Reference fields and add functionality to main panel controller

--- src/main/java/seg/major/controller/SchemaController.java
+++ src/main/java/seg/major/controller/SchemaController.java
@@ -1,6 +1,9 @@
 package seg.major.controller;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.net.URL;
@@ -9,10 +12,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
 import javafx.scene.text.Text;
 import seg.major.App;
 import seg.major.model.SchemaModel;
 import seg.major.structure.Appointment;
+import seg.major.structure.AppointmentEntry;
 import seg.major.structure.Patient;
 import seg.major.structure.User;
 
@@ -23,8 +28,39 @@ public class SchemaController implements Initializable, ViewsController {
 
     private PrimaryController primaryController;
 
+    public TableColumn carriedOverName;
+    public TableColumn carriedOverHospital;
+    public TableColumn carriedOverComplete;
+    public TableColumn carriedOverDueDate;
+    public TableColumn thisWeekName;
+    public TableColumn thisWeekHospital;
+    public TableColumn thisWeekComplete;
+    public TableColumn thisWeekDueDate;
+    public TableColumn mondayName;
+    public TableColumn mondayHospital;
+    public TableColumn mondayComplete;
+    public TableColumn mondayDueDate;
+    public TableColumn tuesdayName;
+    public TableColumn tuesdayHospital;
+    public TableColumn tuesdayComplete;
+    public TableColumn tuesdayDueDate;
+    public TableColumn wednesdayName;
+    public TableColumn wednesdayHospital;
+    public TableColumn wednesdayComplete;
+    public TableColumn wednesdayDueDate;
+    public TableColumn thursdayName;
+    public TableColumn thursdayHospital;
+    public TableColumn thursdayComplete;
+    public TableColumn thursdayDueDate;
+    public TableColumn fridayName;
+    public TableColumn fridayHospital;
+    public TableColumn fridayComplete;
+    public TableColumn fridayDueDate;
+
     public Button under12Btn;
     public Button over12Btn;
+    public boolean isUnder12 = true;
+
     public Text bottomInfo;
 
     private SchemaModel schemaModel;
@@ -65,6 +101,7 @@ public class SchemaController implements Initializable, ViewsController {
       setUpTable();
       setUpButtons();
       setCurrentDate();
+      this.weekText.setText(this.schemaModel.getWeek());
   }
 
     /**
@@ -77,32 +114,113 @@ public class SchemaController implements Initializable, ViewsController {
   }
 
     private void setUpButtons() {
-
+        under12Btn.setOnAction(e -> {
+            under12Btn.setStyle("-fx-background-color: blue;" + "-fx-text-fill: white");
+            over12Btn.setStyle(null);
+            isUnder12 = true;
+            refresh();
+        });
+        over12Btn.setOnAction(e -> {
+            over12Btn.setStyle("-fx-background-color: blue;" + "-fx-text-fill: white");
+            under12Btn.setStyle(null);
+            isUnder12 = false;
+            refresh();
+        });
     }
 
     private void setUpTable() {
-      setUpColumn();
+      setUpColumns();
       setUpRows();
-      //fillTable(schemaModel.getAppointmentsAndPatientsForDayUnder12());
+      fillTablesForUnder12();
       under12Btn.setStyle("-fx-background-color: blue;" + "-fx-text-fill: white");
-      carriedOverTable.setPlaceholder(new Label("No patients found"));
-    }
-
-    public void fillTable(Map<Appointment, Patient> appToPatients) {
-
+      over12Btn.setStyle(null);
     }
 
     private void setUpRows() {
+        mondayTable.setRowFactory(t -> {
+            TableRow<Appointment> row = new TableRow<>();
+            row.setOnMouseClicked(click -> {
+                if (!row.isEmpty() && click.getButton() == MouseButton.PRIMARY && click.getClickCount() == 2) {
+                    Appointment appointment = row.getItem();
+                    //viewPatient(patient);
 
+
+                    UpdateAppointmentController uac = (UpdateAppointmentController) primaryController.getControllerByName(App.updateAppointment);
+                    uac.setData(appointment);
+                    primaryController.setPane(App.updateAppointment);
+
+
+                }
+            });
+            return row;
+        });
     }
 
-    private void setUpColumn(){
-        //((TableColumn)carriedOverTable.getColumns().get(0)).setCellFactory("");
-        /*forename.setCellValueFactory(new PropertyValueFactory("forename"));
-        surname.setCellValueFactory(new PropertyValueFactory("surname"));
-        hospitalNumber.setCellValueFactory(new PropertyValueFactory("hospitalNumber"));
-        localClinic.setCellValueFactory(new PropertyValueFactory("localClinic"));
-        nextAppointment.setCellValueFactory(new PropertyValueFactory("nextAppointment"));*/
+/*    private void fillTable(){
+        patientTable.getItems().clear();
+        for (Patient patient : patientModel.getPatientList()) {
+            patientTable.getItems().add(patient);
+        }
+    }*/
+
+    public void fillTable(TableView tb, List<AppointmentEntry> appEntryList) {
+        tb.getItems().clear();
+        for (AppointmentEntry appEntry : appEntryList) {
+            tb.getItems().add(appEntry);
+        }
+    }
+
+    public void fillTablesForUnder12() {
+        fillTable(mondayTable,schemaModel.getAppointmentsAndPatientsForDayUnder12(DayOfWeek.MONDAY));
+        fillTable(tuesdayTable,schemaModel.getAppointmentsAndPatientsForDayUnder12(DayOfWeek.TUESDAY));
+        fillTable(wednesdayTable,schemaModel.getAppointmentsAndPatientsForDayUnder12(DayOfWeek.WEDNESDAY));
+        fillTable(thursdayTable,schemaModel.getAppointmentsAndPatientsForDayUnder12(DayOfWeek.THURSDAY));
+        fillTable(fridayTable,schemaModel.getAppointmentsAndPatientsForDayUnder12(DayOfWeek.FRIDAY));
+    }
+
+    public void fillTablesForOver12() {
+        fillTable(mondayTable,schemaModel.getAppointmentsAndPatientsForDayOver12(DayOfWeek.MONDAY));
+        fillTable(tuesdayTable,schemaModel.getAppointmentsAndPatientsForDayOver12(DayOfWeek.TUESDAY));
+        fillTable(wednesdayTable,schemaModel.getAppointmentsAndPatientsForDayOver12(DayOfWeek.WEDNESDAY));
+        fillTable(thursdayTable,schemaModel.getAppointmentsAndPatientsForDayOver12(DayOfWeek.THURSDAY));
+        fillTable(fridayTable,schemaModel.getAppointmentsAndPatientsForDayOver12(DayOfWeek.FRIDAY));
+    }
+
+    private void setUpColumns(){
+        carriedOverName.setCellValueFactory(new PropertyValueFactory("name"));
+        carriedOverHospital.setCellValueFactory(new PropertyValueFactory("vnumber"));
+        carriedOverComplete.setCellValueFactory(new PropertyValueFactory("complete"));
+        carriedOverDueDate.setCellFactory(new PropertyValueFactory("dueDate"));
+
+        thisWeekName.setCellValueFactory(new PropertyValueFactory("name"));
+        thisWeekHospital.setCellValueFactory(new PropertyValueFactory("vnumber"));
+        thisWeekComplete.setCellValueFactory(new PropertyValueFactory("complete"));
+        thisWeekDueDate.setCellValueFactory(new PropertyValueFactory("dueDate"));
+
+        mondayName.setCellValueFactory(new PropertyValueFactory("name"));
+        mondayHospital.setCellValueFactory(new PropertyValueFactory("vnumber"));
+        mondayComplete.setCellValueFactory(new PropertyValueFactory("complete"));
+        mondayDueDate.setCellValueFactory(new PropertyValueFactory("dueDate"));
+
+        tuesdayName.setCellValueFactory(new PropertyValueFactory("name"));
+        tuesdayHospital.setCellValueFactory(new PropertyValueFactory("vnumber"));
+        tuesdayComplete.setCellValueFactory(new PropertyValueFactory("complete"));
+        tuesdayDueDate.setCellValueFactory(new PropertyValueFactory("dueDate"));
+
+        wednesdayName.setCellValueFactory(new PropertyValueFactory("name"));
+        wednesdayHospital.setCellValueFactory(new PropertyValueFactory("vnumber"));
+        wednesdayComplete.setCellValueFactory(new PropertyValueFactory("complete"));
+        wednesdayDueDate.setCellValueFactory(new PropertyValueFactory("dueDate"));
+
+        thursdayName.setCellValueFactory(new PropertyValueFactory("name"));
+        thursdayHospital.setCellValueFactory(new PropertyValueFactory("vnumber"));
+        thursdayComplete.setCellValueFactory(new PropertyValueFactory("complete"));
+        thursdayDueDate.setCellValueFactory(new PropertyValueFactory("dueDate"));
+
+        fridayName.setCellValueFactory(new PropertyValueFactory("name"));
+        fridayHospital.setCellValueFactory(new PropertyValueFactory("vnumber"));
+        fridayComplete.setCellValueFactory(new PropertyValueFactory("complete"));
+        fridayDueDate.setCellValueFactory(new PropertyValueFactory("dueDate"));
     }
 
     /**
@@ -130,5 +248,28 @@ public class SchemaController implements Initializable, ViewsController {
 
     public void switchToPatients(ActionEvent event) {
       primaryController.setPane(App.patients);
+    }
+
+    public void previousWeekButtonClicked(ActionEvent event) {
+      this.schemaModel.decrementWeek();
+
+      this.weekText.setText(schemaModel.getWeek());
+      refresh();
+    }
+
+    public void nextWeekButtonClicked(ActionEvent event) {
+      this.schemaModel.incrementWeek();
+
+      this.weekText.setText(schemaModel.getWeek());
+      refresh();
+    }
+
+    public void refresh() {
+      // TODO: check for which button is selected for update.
+      if(isUnder12) {
+          fillTablesForUnder12();
+      }else {
+          fillTablesForOver12();
+      }
     }
 }
