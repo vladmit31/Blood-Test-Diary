cd0cdb933f93349e13af2994484727326d054516
Tom <31593883+TomRYH@users.noreply.github.com>
2019-03-08 11:49:34 +0000

    Begin testing

--- src/test/java/seg/major/AppTest.java
+++ src/test/java/seg/major/AppTest.java
@@ -4,12 +4,37 @@
 package seg.major;
 
 import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import seg.major.controller.PrimaryController;
+
 import static org.junit.Assert.*;
 
-public class AppTest {
-    @Test
-    public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertTrue(true);
+public class AppTest extends ApplicationTest {
+
+    // Name of the view that is shown on first loading the application
+    public static String login = "login";
+    public static String addPatient = "add_patient";
+    public static String patients = "patients";
+    public static String schema = "schema";
+    public static String propertiesLocation = "props.properties";
+    public static Props props;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        PrimaryController primaryController = new PrimaryController();
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        primaryController.addViews(new String[] { login, addPatient, patients, schema });
+        primaryController.setPane(App.login);
+        root.getChildren().addAll(primaryController);
+        primaryStage.setScene(scene);
+        primaryStage.minWidthProperty().bind(primaryController.minWidthProperty());
+        primaryStage.minHeightProperty().bind(primaryController.minHeightProperty());
+        primaryStage.show();
+
     }
 }
---
+++ src/test/java/seg/major/controller/PrimaryControllerTest.java
@@ -0,0 +1,23 @@
+package seg.major.controller;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class GameTeset
+ *
+ * @version 2018.11.15
+ * @authors Yeshvanth Prabakar, Jay Machpherson, Mikes, Tom,
+ */
+
+public class PrimaryControllerTest {
+    public PrimaryControllerTest() {
+    }
+
+    @Before
+    public void init() {
+    }
+
+}
