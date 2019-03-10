302fcbd3c2dbbf1abffe769ae0e2c93f8154d9d6
Horia Pavel <horiapavel@yahoo.com>
2019-03-10 15:28:07 +0000

    Extend App.java for loading contact panel and functionality

--- src/main/java/seg/major/App.java
+++ src/main/java/seg/major/App.java
@@ -15,6 +15,7 @@ public class App extends Application {
     public static String addPatient = "add_patient";
     public static String updatePatient = "update_patient";
     public static String updateAppointment = "update_appointment";
+    public static String contacts = "contacts";
     public static String patients = "patients";
     public static String schema = "schema";
 
@@ -27,7 +28,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         PrimaryController primaryController = new PrimaryController();
-        primaryController.addViews(new String[] { login, addPatient, patients, schema, updatePatient, updateAppointment });
+        primaryController.addViews(new String[] { login, addPatient, patients, schema, contacts, updatePatient, updateAppointment });
         primaryController.setPane(App.login);
 
         Group root = new Group();
