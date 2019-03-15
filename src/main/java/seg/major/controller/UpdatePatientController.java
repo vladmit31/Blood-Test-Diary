f4b50107650b4abe9d82f37832f469c9d944d148
Horia Pavel <horiapavel@yahoo.com>
2019-03-15 17:13:52 +0000

    Update button clicked method

--- src/main/java/seg/major/controller/UpdatePatientController.java
+++ src/main/java/seg/major/controller/UpdatePatientController.java
@@ -6,9 +6,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import seg.major.App;
 import seg.major.model.UpdatePatientModel;
+import seg.major.structure.Appointment;
 import seg.major.structure.Patient;
 import javax.swing.*;
 import java.net.URL;
@@ -62,18 +64,30 @@ public class UpdatePatientController implements Initializable, ControllerInterfa
     /** ---------- FXML ---------- */
 
     @FXML
-    public void updateBtn() {
+    public void updateButtonClicked(MouseEvent mouseEvent) {
         if (checkUserInput()) {
-            Patient p = (Patient) data.get("patient");
-            p.setForename(forenameField.getText());
-            p.setSurname(surnameField.getText());
-            p.setDob(dobField.getValue());
-            p.setNextAppointment(nextAppField.getValue());
-            p.setHospitalNumber(hospitalField.getText());
-            p.setLocalClinic(clinicField.getText());
-            UpdatePatientModel.updatePatient(p);
+
+            Patient patient = (Patient) data.get("patient");
+            patient.setForename(forenameField.getText());
+            patient.setSurname(surnameField.getText());
+            patient.setDob(dobField.getValue());
+            patient.setHospitalNumber(hospitalField.getText());
+            patient.setLocalClinic(clinicField.getText());
+
+            Appointment appointment = (Appointment) data.get("appointment");
+            appointment.setDueDate(nextAppField.getValue());
+
+            UpdatePatientModel.updatePatient(patient);
+            UpdatePatientModel.updateAppointment(appointment);
+
             data.remove("patient");
-            primaryController.sendTo(App.patients, "patient", p);
+            data.remove("appointment");
+
+            disableTextFields();
+
+            primaryController.sendTo(App.patients, "patient", patient);
+            primaryController.sendTo(App.patients, "appointment", appointment);
+
             primaryController.setPane(App.patients);
         } else {
             JOptionPane.showMessageDialog(null, "Complete all fields", "Error", JOptionPane.ERROR_MESSAGE);
@@ -122,6 +136,7 @@ public class UpdatePatientController implements Initializable, ControllerInterfa
     }
 
     public void passPatient(Patient p) {
+        TitleText.setText(p.getForename() + " " + p.getSurname());
         forenameField.setText(p.getForename());
         surnameField.setText(p.getSurname());
         dobField.setValue(p.getDob());
@@ -161,6 +176,10 @@ public class UpdatePatientController implements Initializable, ControllerInterfa
     public void update() {
         if (data.get("patient") != null) {
             passPatient((Patient) data.get("patient"));
+        }
+
+        if (data.get("appointment") != null) {
+            nextAppField.setValue(((Appointment) data.get("appointment")).getDueDate());
         }
     }
 
