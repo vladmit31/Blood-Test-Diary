4e6985b20183693f92c5c30d16da04f54725d49a
Michael Jacob <mottijey@aol.com>
2019-03-25 14:16:32 +0000

    Change test user id

--- src/test/java/seg/major/model/LoginModelTest.java
+++ src/test/java/seg/major/model/LoginModelTest.java
@@ -8,26 +8,23 @@ import org.junit.Test;
 import seg.major.model.database.UserDAO;
 import seg.major.structure.User;
 
-import java.util.List;
 
 public class LoginModelTest {
     @Before
     public void setUp() throws Exception {
         PasswordHash hash = new PasswordHash();
-        UserDAO.create(new User(100,"hello12312313123","hel1312lo@he3231llo.he121llo",hash.createHash("world"),0));
+        UserDAO.create(new User(10000000,"hello12312313123","hel1312lo@he3231llo.he121llo",hash.createHash("world"),0));
     }
 
     @Test
     public void testValidateLogin() {
-       List<User> userList = UserDAO.getAll();
         User firstUser = UserDAO.getByUsername("hello12312313123");
-        LoginModel model = new LoginModel();
-        Assert.assertTrue(model.validateLogin(firstUser.getUsername(),"world"));
-        Assert.assertFalse(model.validateLogin(firstUser.getUsername(),"world1"));
+        Assert.assertTrue(LoginModel.validateLogin(firstUser.getUsername(),"world"));
+        Assert.assertFalse(LoginModel.validateLogin(firstUser.getUsername(),"world1"));
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
         UserDAO.remove(UserDAO.getByUsername("hello12312313123"));
     }
 }
