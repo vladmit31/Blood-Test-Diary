beb3bd5be14485a9d3915d15890ad3f7283e2c74
Michael Jacob <mottijey@aol.com>
2019-03-25 13:28:27 +0000

    Use static access for create patient

--- src/test/java/seg/major/model/AddPatientModelTest.java
+++ src/test/java/seg/major/model/AddPatientModelTest.java
@@ -19,8 +19,7 @@ public class AddPatientModelTest {
 
     @Test
     public void createPatientTest() {
-        AddPatientModel model = new AddPatientModel();
-        model.createPatient( "Person","One", LocalDate.parse("2000-02-13"),"4252","London","",1.0,LocalDate.now().plusWeeks(1),"","","");
+        AddPatientModel.createPatient( "Person","One", LocalDate.parse("2000-02-13"),"4252","London","",1.0,LocalDate.now().plusWeeks(1),"","","");
         Map<String,String> patientMap = new HashMap();
         patientMap.put("vnumber","4252");
         Patient patient = PatientDAO.get(patientMap);
